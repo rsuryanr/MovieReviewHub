a9eff539edab81dcd75a6a814494b9340931b81b
Surya Ramireddy <96761396+rsuryanr@users.noreply.github.com>
2024-04-28 20:54:15 +0530

    Add files via upload

---
+++ MovieReviewHub-backend/src/main/java/dev/surya/movielist/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.surya.movielist.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
